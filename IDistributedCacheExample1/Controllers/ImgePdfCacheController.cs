785b69c73422cefd5a29241abdff4eee8feca6cd
Barış Avtan <baris.avtan@arvento.com>
2023-01-30 14:51:40 +0300

    second commit.

---
+++ IDistributedCacheExample1/Controllers/ImgePdfCacheController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheExample1.Controllers
+{
+    public class ImgePdfCacheController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public ImgePdfCacheController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult ImageCache()
+        {
+            string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/car1.jpeg");
+            
+            byte[] imageByte = System.IO.File.ReadAllBytes(imagePath);
+            
+            _distributedCache.Set("resim1", imageByte);
+            
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            //ImageUrl'i bu sayfada gösterelim
+            return View();
+
+
+        }
+
+        public IActionResult ImageUrl()
+        {
+
+            byte[] imageByte = _distributedCache.Get("resim1");
+
+            return File(imageByte, "imege/jpeg");
+        }
+    }
+}
--- IDistributedCacheExample1/Controllers/ProductController.cs
+++ IDistributedCacheExample1/Controllers/ProductController.cs
@@ -1,35 +1,95 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheExample1.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace IDistributedCacheExample1.Controllers
 {
     public class ProductController : Controller
     {
-        private IDistributedCache _distribbutedCache;
+        private IDistributedCache _distributedCache;
 
         public ProductController(IDistributedCache distribbutedCache)
         {
-            _distribbutedCache = distribbutedCache;
+            _distributedCache = distribbutedCache;
         }
         public IActionResult Index()
         {
             DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
             cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            _distribbutedCache.SetString("name", "Baris", cacheEntryOptions);//bu keye sahip olan değer memory'de 1 dk duracak.
+            _distributedCache.SetString("name", "Baris", cacheEntryOptions);//bu keye sahip olan değer memory'de 1 dk duracak.
             return View();
         }
 
 
-        public async Task<IActionResult> Index2()
+        public async Task<IActionResult> IndexOne()
         {
             DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
             cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
-            await _distribbutedCache.SetStringAsync("surname", "Dogan", cacheEntryOptions);//bu keye sahip olan değer memory'de 1 dk duracak.
+            await _distributedCache.SetStringAsync("surname", "Dogan", cacheEntryOptions);//bu keye sahip olan değer memory'de 1 dk duracak.
+            return View();
+        }
+
+        public async Task<IActionResult> IndexTwo()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+            
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            Product product = new Product { Id = 1 ,Name = "Kalem", Price=100};
+
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            await _distributedCache.SetStringAsync("product:1" , jsonProduct, cacheEntryOptions);
+
+            return View();
+        }
+
+        public IActionResult ShowIndexTwo()
+        {
+            string jsonProduct = _distributedCache.GetString("product:1");
+            Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            ViewBag.product = p;
+            return View();
+
+        }
+
+        public async Task<IActionResult> ByteExample()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 100 };
+           
+            string jsonProduct = JsonConvert.SerializeObject(product);
+            
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+
+            _distributedCache.Set("product:1", byteProduct);
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> ByteConvertString()
+        {
+
+
+            Byte[] byteProduct = _distributedCache.Get("product:1");
+
+            string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+
+            Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+            ViewBag.product = p;
+
             return View();
         }
         public IActionResult Show()
         {
-            string name = _distribbutedCache.GetString("name");
+            string name = _distributedCache.GetString("name");
             ViewBag.name = name;
             return View();
 
@@ -37,9 +97,10 @@ namespace IDistributedCacheExample1.Controllers
 
         public IActionResult RemoveData()
         {
-            _distribbutedCache.Remove("name");
+            _distributedCache.Remove("name");
             return View();
 
         }
+
     }
 }
