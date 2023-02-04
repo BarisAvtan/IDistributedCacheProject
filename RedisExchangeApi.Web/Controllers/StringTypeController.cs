f0c2454f060e826f9103a44bddf2cc8d1ca77447
Barış Avtan <baris.avtan@arvento.com>
2023-02-04 18:14:30 +0300

    StackExchange.Redis API commit

---
+++ RedisExchangeApi.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeApi.Web.Services;
+
+namespace RedisExchangeApi.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService= redisService;
+        }
+        public IActionResult Index()
+        {
+            _redisService.Connect();
+            var db = _redisService.GetDb(0);
+            db.StringSet("name", "Baris BD");
+            db.StringSet("name2", 100);
+            return View();
+        }
+    }
+}
---
+++ RedisExchangeApi.Web/Services/RedisService.cs
@@ -0,0 +1,33 @@
+﻿using StackExchange.Redis;
+
+namespace RedisExchangeApi.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        public IDatabase db { get; set; }
+
+        private ConnectionMultiplexer _redis;//ConnectionMultiplexer clası üzerinden redis ile haberleşilir.
+
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+
+            _redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()//uygulama ayağa kalktıgında bu connect metodu çagrılmalı yani program.cs 'e eklenmeli
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+
+            _redis = ConnectionMultiplexer.Connect(configString);
+
+        }
+
+        public IDatabase GetDb(int dbNumber) //redis serverda 1 den 15 'e kadar olan dblerden birini şeçebilmek için yazıldı.
+        {
+            return _redis.GetDatabase(dbNumber);
+        }
+    }
+}
